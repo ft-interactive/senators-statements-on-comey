a48e78e8b73df67328bbcd84cd22616aaad7a863
Lauren Leatherby <lauren.leatherby@ft.com>
2017-05-12 11:38:11 -0400

    fix bertha issue

--- config/article.js
+++ config/article.js
@@ -9,7 +9,7 @@ export default () => ({ // eslint-disable-line
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date(),
+  publishedDate: new Date('2017-05-11T23:00:00Z'),
 
   headline: 'How Senators reacted to Comey dismissal',
 
